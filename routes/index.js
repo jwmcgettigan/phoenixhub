d493bacc5e7599f948f8edd9bcf0d156e135b433
Justin McGettigan <jwmcgettigan@gmail.com>
2018-04-12 16:09:10 -0400

    Added some pages.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('pages/index', { title: 'Express' });
 });
 
+/* GET news page. */
+router.get('/news', function(req, res, next) {
+  res.render('pages/news', { title: 'Express' });
+});
+
+/* GET events page. */
+router.get('/events', function(req, res, next) {
+  res.render('pages/events', { title: 'Express' });
+});
+
+/* GET clubs page. */
+router.get('/clubs', function(req, res, next) {
+  res.render('pages/clubs', { title: 'Express' });
+});
+
 module.exports = router;
