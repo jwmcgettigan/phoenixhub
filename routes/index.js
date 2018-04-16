b59350cad7810dd288322f7949c830f031215ea0
Justin McGettigan <jwmcgettigan@gmail.com>
2018-04-16 17:46:17 -0400

    Have decided to use KeystoneJS as our CMS and platform of choice for developing the website.

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,46 @@
-var express = require('express');
-var router = express.Router();
+/**
+ * This file is where you define your application routes and controllers.
+ *
+ * Start by including the middleware you want to run for every request;
+ * you can attach middleware to the pre('routes') and pre('render') events.
+ *
+ * For simplicity, the default setup for route controllers is for each to be
+ * in its own file, and we import all the files in the /routes/views directory.
+ *
+ * Each of these files is a route controller, and is responsible for all the
+ * processing that needs to happen for the route (e.g. loading data, handling
+ * form submissions, rendering the view template, etc).
+ *
+ * Bind each route pattern your application should respond to in the function
+ * that is exported from this module, following the examples below.
+ *
+ * See the Express application routing documentation for more information:
+ * http://expressjs.com/api.html#app.VERB
+ */
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'Express' });
-});
+var keystone = require('keystone');
+var middleware = require('./middleware');
+var importRoutes = keystone.importer(__dirname);
 
-/* GET news page. */
-router.get('/news', function(req, res, next) {
-  res.render('pages/news', { title: 'Express' });
-});
+// Common Middleware
+keystone.pre('routes', middleware.initLocals);
+keystone.pre('render', middleware.flashMessages);
 
-/* GET events page. */
-router.get('/events', function(req, res, next) {
-  res.render('pages/events', { title: 'Express' });
-});
+// Import Route Controllers
+var routes = {
+	views: importRoutes('./views'),
+};
 
-/* GET clubs page. */
-router.get('/clubs', function(req, res, next) {
-  res.render('pages/clubs', { title: 'Express' });
-});
+// Setup Route Bindings
+exports = module.exports = function (app) {
+	// Views
+	app.get('/', routes.views.index);
+	app.get('/blog/:category?', routes.views.blog);
+	app.get('/blog/post/:post', routes.views.post);
+	app.get('/gallery', routes.views.gallery);
+	app.all('/contact', routes.views.contact);
 
-module.exports = router;
+	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
+	// app.get('/protected', middleware.requireUser, routes.views.protected);
+
+};
